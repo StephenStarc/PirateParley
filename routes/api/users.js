051f9dc6a89926e77c72764a726340dd044422c8
ModernEvangelism1 <144702766+StephenStarc@users.noreply.github.com>
2024-04-12 17:29:17 +0530

    Added Custom Middleware

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,12 +1,13 @@
 const express = require('express')
 const router = express.Router()
-const {check , validationResult} = require('express-validator') //Finished here
-const User = require('../../models/User')
+const {check , validationResult} = require('express-validator')
+const jwt = require('jsonwebtoken')
+const User = require('../../models/User') //importing Model
 const gravatar = require('gravatar')
 const bcrypt = require('bcryptjs')
-
+const config = require('config')
 // @route POST api/users
-// @desc Register USer Route
+// @desc Register User Route
 // @access Public
 router.post('/',[
     check('name','Name is Required').not().isEmpty(),
@@ -14,26 +15,23 @@ router.post('/',[
     check('password','Please enter a Password with 6 or more characters').isLength({min:6})
 ],async (req,res)=>{
     const errors = validationResult(req)
+    
     if(!errors.isEmpty()){
         return res.status(400).json({errors: errors.array()})
     }
-    const {name, email, password} = req.body
+    const {name, email, password} = req.body //destructuring values from req.body
 
-    
 try{
+
     let user = await User.findOne({email})
-
     if(user){
-        return res.status(400).json({errors:[{msg:'User already Exists'}]})
+        return res.status(400).json({errors:[{msg:'User already Exists'}]}) //if user already exists setting msg value
     }
-
-    const avatar = gravatar.url(email, {
-        s:'200',
+    const avatar = gravatar.url(email, { //gravator icon in email
+        s:'200', 
         r:'pg',
         d:'mm'
     })
-
-
     user = new User({
         name,
         email,
@@ -43,19 +41,28 @@ try{
 
     const salt = await bcrypt.genSalt(10)
 
-    user.password = await bcrypt.hash(password, salt)
+    user.password = await bcrypt.hash(password, salt) //modifing user.password after initiating
 
     await user.save()
-    res.send('User Registered')
+   
+    const playload = {
+        user:{
+            id:user.id //get id from promise returned by  - await user.save()
+        }
+    }
+
+    jwt.sign(playload,
+        config.get('jwtSecret'),
+        {expiresIn:360000},
+        (err, token) =>{
+            if(err) throw err
+            res.json({token})
+        }
+    )
 }catch(err){
     console.error(err.message)
     res.status(500).send('Server Error')
 }
-
-
-
-
-
 
 })
 
